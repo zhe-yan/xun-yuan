76abc8c193c6e5a328cb820a46a95ac0b3a593f0
Echo <unknown@example.com>
2020-01-19 18:53:41 +0800

    2

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,18 +1,25 @@
 // pages/my/my.js
+var environment = require("../../module/environment").environment;
+var tetris = require("../../module/tetris").tetris;
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    "useravatar":"https://lover-match.oss-cn-hangzhou.aliyuncs.com/DEV%2FFemale%2FAvatar%2F5_UserBasicInfo6_HeadPhoto.jpg",
+    "function":["注册资料", "基本资料", "诚信资料", "隐私资料", "择偶条件", "个性问答", "偏好", "文章", "视频", "相册", ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      vubi: tetris.storageTool.getString("vubi")
+    })
+    console.log(this.data.vubi)
   },
 
   /**
